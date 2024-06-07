976ec9b8a32221a5dace213bebcc28978e4915b8
Đinh Gia Bảo <107354883+RankillerDY@users.noreply.github.com>
2024-06-07 15:44:36 +0700

    CartScreen Header and BlockItems

--- src/routes/MainStack/index.tsx
+++ src/routes/MainStack/index.tsx
@@ -6,6 +6,7 @@ import UserStack from "./UserStack";
 import BottomStack from "../BottomStack";
 import CourseDetailsScreen from "../../screens/CourseDetails";
 import CourseDetailStack from "./CourseDetailStack";
+import CartStack from "./CartStack";
 const Stack = createStackNavigator();
 
 export default function MainStack() {
@@ -16,13 +17,14 @@ export default function MainStack() {
       }}
       initialRouteName={nameScreen.HOMEPAGE}
     >
-      <Stack.Screen name={nameScreen.HOMEPAGE} component={BottomStack} />
+      {/* <Stack.Screen name={nameScreen.HOMEPAGE} component={BottomStack} />
       <Stack.Screen name={nameScreen.BLOGSTACK} component={BlogStack} />
       <Stack.Screen name={nameScreen.USER_STACK} component={UserStack} />
       <Stack.Screen
         name={nameScreen.COURSEDETAILS}
         component={CourseDetailStack}
-      />
+      /> */}
+      <Stack.Screen name={nameScreen.CART} component={CartStack} />
     </Stack.Navigator>
   );
 }
